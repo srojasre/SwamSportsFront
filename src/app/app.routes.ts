0dfec229816aa338e9597be359b7b105abdac444
srojasre <s.rojasr2@uniandes.edu.co>
2024-12-23 01:38:33 -0500

    main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component: HomeComponent, title: 'Home Page'}
+    // Demás rutas a implementar 
+];
