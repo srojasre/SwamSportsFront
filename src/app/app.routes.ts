e88bbec18ea39adbf9a17fb184f68ab0169f7360
srojasre <s.rojasr2@uniandes.edu.co>
2024-12-26 17:34:42 -0500

    Downgrade angular 17

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-
-export const routes: Routes = [
-    {path:'home', component: HomeComponent, title: 'Home Page'}
-    // Demás rutas a implementar 
-];
--- swamsportsFront/src/app/app.component.spec.ts
+++ swamsportsFront/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'SwamSportsFront' title`, () => {
+  it(`should have the 'swamsportsFront' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SwamSportsFront');
+    expect(app.title).toEqual('swamsportsFront');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SwamSportsFront');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, swamsportsFront');
   });
 });
